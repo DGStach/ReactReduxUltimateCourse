5504d55c3e8c28037024dd353cdcb74b65fc47fc
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-03 14:41:52 +0200

    advice function part 1 - set state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import {useState} from "react";
 
 function App() {
-    const [advice, setadvice] = useState("")
+    const [advice, setadvice] = useState("");
+    const [count, setCount] = useState(0);
   async function getAdvice(){
     const res = await fetch("https://api.adviceslip.com/advice")
     const data = await  res.json()
       setadvice(data.slip.advice);
+    setCount((c)=>c+1)
   }
   return (
     <div className="App">
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
+        <p>
+            You have read <strong>{count}</strong> pieces of advaice.
+        </p>
     </div>
   );
 }
