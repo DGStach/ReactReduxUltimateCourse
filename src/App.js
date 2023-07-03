6e43ca4010bc8cbda4094975d56db09cd9e7620d
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-03 14:34:43 +0200

    advice function

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {useState} from "react";
+
+function App() {
+    const [advice, setadvice] = useState("")
+  async function getAdvice(){
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await  res.json()
+      setadvice(data.slip.advice);
+  }
+  return (
+    <div className="App">
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+    </div>
+  );
+}
+
+export default App;
